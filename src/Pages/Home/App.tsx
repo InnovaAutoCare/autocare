ee1f795c11f50049a3e858a7794db2c65363f782
edvxndxvi <edvandavi1@hotmail.com>
2024-09-08 21:46:45 -0300

    Adicionando Footer a page Home e alterando fonte de titulos do footer

--- src/Pages/Home/App.tsx
+++ src/Pages/Home/App.tsx
@@ -1,3 +1,4 @@
+import Footer from '../../components/Footer'
 import Header from '../../components/Header'
 import './App.css'
 
@@ -5,6 +6,8 @@ function App() {
   return (
     <>
       <Header/>
+      
+      <Footer/>
     </>
   )
 }
