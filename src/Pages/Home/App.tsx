81cbba1c468c7b597a1a3acb1667210bc6bc5710
viniciuslabernardes <vinicius.l.a.bernardes@outlook.com>
2024-09-11 17:07:58 -0300

    Estruturação da section Sobre

--- src/Pages/Home/App.tsx
+++ src/Pages/Home/App.tsx
@@ -16,7 +16,19 @@ function App() {
         <div className="banner-main">
             <div className="imagem-main"></div>
         </div>
-    </main>
+      </main>
+
+      <section className="bg-servico" id="sobre-serviço" aria-label="Sobre o serviço">
+        <div className="servico">
+            <div className="texto-servico container">
+                <h2 className="h2">Por que utilizar o <span>serviço AutoCare</span>?</h2>
+                <p>Nossa tecnologia inovadora simplifica processos e melhora a eficiência nos centros automotivos.</p>
+            </div>
+            <div className="cards-servico">
+            </div>
+            <Button conteudo='CONHECER O SERVIÇO'/>
+        </div>
+    </section>
       <Footer/>
     </>
   )
