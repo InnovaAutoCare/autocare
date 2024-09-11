de356b7ec920e355051d5e3109f2c9cdadf6c565
edvxndxvi <edvandavi1@hotmail.com>
2024-09-11 11:07:37 -0300

    Estruturação do Main

--- src/Pages/Home/App.tsx
+++ src/Pages/Home/App.tsx
@@ -1,3 +1,4 @@
+import Button from '../../components/Button'
 import Footer from '../../components/Footer'
 import Header from '../../components/Header'
 import './App.css'
@@ -6,7 +7,16 @@ function App() {
   return (
     <>
       <Header/>
-      
+      <main className="container">
+        <div className="texto-main">
+            <h1>Uma nova maneira de <span>diagnosticar</span></h1>
+            <p>Tenha o diagnóstico do problema do seu veículo de uma nova maneira.</p>
+            <Button conteudo='CONHECER o SERVIÇO'/>
+        </div>
+        <div className="banner-main">
+            <div className="imagem-main"></div>
+        </div>
+    </main>
       <Footer/>
     </>
   )
