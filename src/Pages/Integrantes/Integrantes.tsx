fbf8a68b263981415e30a3d0f98d02fbc37ed5de
rafaelromanini <rafaromaninideoliveira@gmail.com>
2024-09-13 22:41:16 -0300

    Estilizando componente Card Integrantes

--- src/Pages/Integrantes/Integrantes.tsx
+++ src/Pages/Integrantes/Integrantes.tsx
@@ -5,6 +5,11 @@ import styleButton from '../../components/Button/index.module.css'
 import githubIcon from '../../assets/svg/github-icon-footer.svg'
 import Footer from '../../components/Footer'
 import Header from '../../components/Header'
+import CardIntegrante from '../../components/CardIntegrante'
+import edvanGithub from '../../assets/images/edvan-github.jpg'
+import rafaelGithub from '../../assets/images/rafael-github.jpg'
+import viniciusGithub from '../../assets/images/vinicius-github.jpg'
+
 
 function Integrantes() {
   return (
@@ -14,6 +19,14 @@ function Integrantes() {
       <section className={`${style.main_integrantes} ${styleIndex.container}`} aria-label="Integrantes">
         <h1>Equipe <span>Auto</span>Care</h1>
         <ul>
+          <CardIntegrante
+            foto={edvanGithub}
+            nome='Edvan Davi'
+            rm='RM554733'
+            turma='Turma TDSPZ'
+            linkGithub='https://github.com/edvxndxvi'
+            linkLinkedin='https://www.linkedin.com/in/edvan-davi-119970236/'
+          />
         </ul>
         <a href="https://github.com/InnovaAutoCare/autocare" className={`${styleButton.button} ${style.rep_link}`} target="_blank">Repositório Git<img src={githubIcon} alt="" /></a>
       </section>
--- src/components/CardIntegrante/index.tsx
+++ src/components/CardIntegrante/index.tsx
@@ -1,9 +1,12 @@
 import './index.css';
+import githubIcon from '../../assets/svg/github-icon-integrantes.svg'
+import linkedinIcon from '../../assets/svg/linkedin-icon-integrantes.svg'
 
 function CardIntegrante(props:{
     foto: string,
     nome: string,
     rm: string,
+    turma: string,
     linkGithub: string,
     linkLinkedin: string
 }) {
@@ -12,10 +15,10 @@ function CardIntegrante(props:{
         <li className="card_integrante">
             <img src={props.foto} alt="" className="profile-pic"/>
             <h3>{props.nome}</h3>
-            <p>{props.nome}</p>
+            <p>{props.nome} - {props.turma}</p>
             <div className="redes-integrantes">
-                <a href={props.linkGithub} target="_blank"><img src="assets/svg/github-icon-integrantes.svg" alt="Logo Github"/></a>
-                <a href={props.linkLinkedin} target="_blank"><img src="assets/svg/linkedin-icon-integrantes.svg" alt="Logo LinkedIn"/></a>
+                <a href={props.linkGithub} target="_blank"><img src={githubIcon} alt="Logo Github"/></a>
+                <a href={props.linkLinkedin} target="_blank"><img src={linkedinIcon} alt="Logo LinkedIn"/></a>
             </div>
         </li>
     </>
