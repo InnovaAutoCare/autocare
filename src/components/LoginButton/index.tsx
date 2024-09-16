f659ce08424b792d885638909d23d31b0ffbae3c
edvxndxvi <edvandavi1@hotmail.com>
2024-09-15 21:21:26 -0300

    Estruturando componente LoginButton

--- src/components/LoginButton/index.tsx
+++ src/components/LoginButton/index.tsx
@@ -1,9 +1,11 @@
 import './index.css'
 
-function LoginButton() {
+function LoginButton(props: {
+    img: string
+}) {
   return (
     <>
-       
+       <li><a href=""><img src={props.img} alt=""/></a></li>
     </>
   )
 }
